26880d0fd2573a75b02a5db844eb1206332c5b5b
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-16 23:05:34 +0100

    contaktZurAPI

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,63 @@
 console.log("test");
 
+/* 
+
+API
+https://picsum.photos/v2/list
+
+author und bild holen
+dann in figure rein
+
+dann ausgeben
+
+css -> img-Tag  width von 50%
+
+einen Loop
+appendChild
+
+*/
+
+
+
+/* 
+
+<figure>
+	<img src="https://picsum.photos/id/0/5616/3744">
+	<figcaption>Alejandro Escamilla</figcaption>
+</figure>
+
+*/
+
+// mit fetch auf API zugreifen
+fetch("https://picsum.photos/v2/list")
+.then(response => response.json())
+.then((data) => {
+
+	data.forEach((bild_Object) =>{
+
+console.log(bild_Object);
+
+/* 
+das kommt von der API an 
+
+Object
+
+author : "Alejandro Escamilla"
+download_url : "https://picsum.photos/id/0/5000/3333"
+height : 3333
+id : "0"
+url : "https://unsplash.com/photos/yC-Yzbqy7PY"
+width : 5000
+[[Prototype]] : Object
+
+*/
+
+
+
+		// !!! Ende forEach 
+	})
+
+	// !!! Ende data fetch
+})
+
+
