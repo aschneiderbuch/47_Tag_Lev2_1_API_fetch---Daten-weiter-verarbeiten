d4119bdb9d6f95964632d1e9affef7ee51b8c92a
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-17 01:07:54 +0100

    fetchArrayVerzögert

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,8 @@ appendChild
 
 */
 
+const download_url_Array = [];
+
 // mit fetch auf API zugreifen
 fetch("https://picsum.photos/v2/list")
 	.then(response => response.json())
@@ -60,6 +62,8 @@ fetch("https://picsum.photos/v2/list")
 					<figcaption>Alejandro Escamilla</figcaption>
 				</figure>
 			
+				<a href=""></a>
+
 			*/
 
 			// in Var speichern     id author download_url 
@@ -75,22 +79,81 @@ fetch("https://picsum.photos/v2/list")
 			// erzeugen
 			// figure  erzeugen
 			const contactElement_figure = document.createElement('figure');
+			// input/innerHTML zuweisen   - hat keinen
+
 			// figcaption erzeugen
 			const contactElement_figcaption = document.createElement('figcaption');
+			// input/innerHTML zuweisen   -  author
+			contactElement_figcaption.innerHTML = author;
+
 			// img erzeugen
 			const contactElement_img = document.createElement('img');
-			// img    Attribute hinzufügen
+			// img  input zuweisen/innerHMTL   bzw.   Attribute hinzufügen
 			// img    Attribute    src   hinzufügen
 			contactElement_img.src = download_url
 			// img    Attribute    alt   hinzufügen
 			contactElement_img.atr = `Bild id ${id} vom Author ${author}`;
 
 
+			// !! image in a-Tag rein  und dem a-Tag download_url geben, damit es beim anklicken großes bild gibt
+			// cursor: pointer  - es kommt finger, wenn man über Bild geht
+			// a-Tag erzeugen
+			const contactElement_aTag = document.createElement('a');
+			// a-Tag input/innerHTML zuweisen    - hat nix
+			// Attribute href
+			// ! contactElement_aTag.href = `${download_url}`;
+			// Attribute target   _blank,    -> damit neues Fenster aufgeht
+			// ! contactElement_aTag.target = '_blank';
+			// Attribute   id   mitgeben, damit man click mit eventListener später abfangen kann
+			contactElement_aTag.id = `${id}`;
+			download_url_Array.push(download_url);
+			console.log(download_url_Array);
+
+
+			// html bauen    und    atag        sollen rein als kinder   img 
+			// !!! deshalb unten img      in //
+			contactElement_aTag.appendChild(contactElement_img);
+
+			// html struktur bauen  bzw. verschachtenl    kind 
+			// figure      sollen rein  als kinder     img  und figcaption
+			// !!! contactElement_figure.appendChild(contactElement_img);
+			contactElement_figure.appendChild(contactElement_aTag);           // !!!   hier  aTag   anstatt img rein, da img ja im aTag ist
+			contactElement_figure.appendChild(contactElement_figcaption);
+
+
+			// jetzt ausgabe und sichtbar machen im body     als kind 
+			document.body.appendChild(contactElement_figure);
+
+
 
 			// !!! Ende forEach 
 		})
+		console.log(download_url_Array);
 
 		// !!! Ende data fetch
 	})
 
+// es kommt nichts an, da die Daten asyncron geladen werden 
+// der fetch braucht zu lange, und der restliche code wird weiter ausgeführt, bis dann mal der rest da ist :-(
+
+console.log(download_url_Array);
+
+// Timeout wertet 1 Sek und ruft dann erst Array von fetch API ab, somit sind die Array daten da.
+setTimeout(function () {
+	console.log(download_url_Array)
+	download_url_Array
+
+
+	const fiktiverBtnImg = document.querySelector('img');
+
+	if (fiktiverBtnImg) {
+		fiktiverBtnImg.addEventListener('click', function (event) {
+			console.log("in event listener");
+			console.log(event);
+
+		})
+	}
+	
+
+}, 1000)
 
